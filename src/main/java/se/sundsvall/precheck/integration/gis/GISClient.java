12dfaa392299d2ab62907b9222e5b66062dfad93
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-03 11:26:00 +0100

    UF-4484: Adjusted circuit breaker settings
    - Removed circuitbreaker annotation on feign clients
    - Updated Dept44 to version 1.30

--- src/main/java/se/sundsvall/precheck/integration/gis/GISClient.java
+++ src/main/java/se/sundsvall/precheck/integration/gis/GISClient.java
@@ -1,18 +1,17 @@
 package se.sundsvall.precheck.integration.gis;
 
-import generated.se.sundsvall.gisapi.PreCheck200Response;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import se.sundsvall.precheck.integration.gis.configuration.GISConfiguration;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static se.sundsvall.precheck.integration.gis.configuration.GISConfiguration.CLIENT_REGISTRATION_ID;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import generated.se.sundsvall.gisapi.PreCheck200Response;
+import se.sundsvall.precheck.integration.gis.configuration.GISConfiguration;
+
 
 @FeignClient(name = CLIENT_REGISTRATION_ID, url = "${integration.gis.url}", configuration = GISConfiguration.class)
-@CircuitBreaker(name = CLIENT_REGISTRATION_ID)
 public interface GISClient {
 
 	@GetMapping(path = "/precheck", produces = APPLICATION_JSON_VALUE)
